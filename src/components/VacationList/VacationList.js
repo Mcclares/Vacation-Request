b7de306e76cbe0d46a61e5622df92b8639616d79
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-17 19:23:23 +0300

    feat(components): add basic table for list using MUI

---
+++ src/components/VacationList/VacationList.js
@@ -0,0 +1,32 @@
+﻿import {Table, TableHead, TableCell, TableBody} from "@mui/material";
+import {TableRow} from "@mui/material";
+
+import React from "react";
+
+
+export default function VacationList() {
+    const[requests, setRequests] = useState([])
+    
+    
+    return(
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Start date</TableCell>
+                    <TableCell>Vacation days</TableCell>
+                    <TableCell>End date</TableCell>
+                    <TableCell>Comment</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {requests.map((request) => (
+                    <TableRow>
+                        <TableCell>{request.startDate}</TableCell>
+                        <TableCell>{request.vacDays}</TableCell>
+                        <TableCell>{request.endDate}</TableCell>
+                        <TableCell>{request.comment}</TableCell>
+                    </TableRow>
+                    ))}
+            </TableBody>
+        </Table>)
+}
