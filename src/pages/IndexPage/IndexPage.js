2cb1e01a2ec649db840a9f697804bc0f92a92b9b
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-20 23:13:53 +0300

    feat(IndexPage): implement button for submitting request

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,10 +1,11 @@
 ﻿import VacationList from "../../components/VacationList/VacationList";
 import "./IndexPage.css";
-
+import CustomButton from "../../components/Button/СustomButton";
 export default function IndexPage() {
     return (
         <div className="vacations-list">
             <VacationList/>
+            <CustomButton name="Submit request" variant="contained" />
         </div>
     )
 }
