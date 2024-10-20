bbe1b98c214d33590802a1033a07e8fc80dd2272
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 00:28:03 +0300

    feat(utils): create handler for navigation to another page

---
+++ src/utils/handleNavigation.js
@@ -0,0 +1,8 @@
+﻿import {useHistory} from 'react-router-dom';
+export function handleNavigation() {
+   const history = useHistory();
+   const navigate = (path) => {
+      history.push(path)
+   };
+   return {navigate}
+}
