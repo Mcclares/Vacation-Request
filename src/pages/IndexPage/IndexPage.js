df6c42a175e564c3a1aad293647eda0a7a7997a6
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-20 21:20:06 +0300

    style(components): add className attribute for styling

---
+++ src/pages/IndexPage/IndexPage.js
@@ -0,0 +1,10 @@
+﻿import VacationList from "../../components/VacationList/VacationList";
+
+
+export default function IndexPage() {
+    return (
+        <div className="vacations-list">
+            <VacationList/>
+        </div>
+    )
+}
