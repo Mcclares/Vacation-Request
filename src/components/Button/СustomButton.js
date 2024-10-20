39265486d4dde8fa98a5d5cb9647da90421e7ed8
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-20 23:03:27 +0300

    feat(components): create customButton using Mui

---
+++ src/components/Button/СustomButton.js
@@ -0,0 +1,6 @@
+﻿import {Button as MuiButton} from "@mui/material";
+export default function CustomButton({name,clickEvent,variantStyle,customStyle}) {
+    return(
+        <MuiButton style={customStyle || ""}  variant={variantStyle} onClick={clickEvent}>{name}</MuiButton>
+    )
+}
