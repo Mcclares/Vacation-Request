f0d828f80f255b98219029863dae0db5803e1279
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 00:29:14 +0300

    feat(IndexPage): implement navigation handler to button

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,11 +1,14 @@
 ﻿import VacationList from "../../components/VacationList/VacationList";
 import "./IndexPage.css";
 import CustomButton from "../../components/Button/СustomButton";
+import {handleNavigation} from "../../utils/handleNavigation";
+
 export default function IndexPage() {
+    const {navigate} = handleNavigation()
     return (
         <div className="vacations-list">
             <VacationList/>
-            <CustomButton name="Submit request" variant="contained" />
+            <CustomButton name="Submit request" variant="contained" clickEvent={()=> navigate("/requestForm")} />
         </div>
     )
 }
