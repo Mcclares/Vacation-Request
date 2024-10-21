efffe22cf7e9012fc66dab7a60a32c293eefc773
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 16:57:57 +0300

    feat(components): add state management for vacation dates and days using custom hook

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -1,4 +1,4 @@
-﻿
+﻿import {useState} from "react";
 import {FormControl, InputLabel, TextField} from "@mui/material";
 import {DatePicker} from "@mui/x-date-pickers";
 import {LocalizationProvider} from "@mui/x-date-pickers";
@@ -6,15 +6,44 @@ import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import CustomNumberInput from "../CustomNumberInput/CustomNumberInput";
 import {FieldStyle} from "./RequestFormStyle";
 import dayjs from "dayjs";
+import useVacationDays from "../../hooks/useVacationDays";
 export default function RequestForm() {
     const today = dayjs();
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [vacationDays, setVacationDays] = useVacationDays(startDate,endDate)
+    
+    const handleStartDateChange = (newDate) => {
+        setStartDate(newDate);
+    }
+    const handleEndDateChange = (newDate) => {
+        setEndDate(newDate)
+    }
+    const handleVacationDaysChange = (event) => {
+        const days = parseInt(event.target.value, 10 ) || 0 ;
+        setVacationDays(days);
+    }
     
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <FormControl >
-                <DatePicker sx={FieldStyle} label="Start date" minDate={today} required/>
-                <CustomNumberInput sx={FieldStyle}  label="Vacation days" required/>
-                <DatePicker sx={FieldStyle}  label="End date" minDate={today} required></DatePicker>
+                <DatePicker 
+                    sx={FieldStyle} 
+                    label="Start date" 
+                    minDate={today} 
+                    onChange={handleStartDateChange}
+                    required/>
+                <CustomNumberInput 
+                    sx={FieldStyle}  
+                    label="Vacation days"
+                    onChange={handleVacationDaysChange}
+                    required/>
+                <DatePicker 
+                    sx={FieldStyle}  
+                    label="End date" 
+                    minDate={today}
+                    onChange={handleEndDateChange}
+                    required></DatePicker>
                 <TextField 
                     multiline
                     rows={5} 
