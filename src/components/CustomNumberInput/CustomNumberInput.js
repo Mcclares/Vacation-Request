e75f58d7dcdcad0574986de7893cf0f1789b2719
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 13:44:02 +0300

    feat(components): add label for CustomInputField and change aria-label for clarity

--- src/components/CustomNumberInput/CustomNumberInput.js
+++ src/components/CustomNumberInput/CustomNumberInput.js
@@ -4,31 +4,43 @@ import { Unstable_NumberInput as BaseNumberInput } from '@mui/base/Unstable_Numb
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
 import {StyledButton,StyledInput,StyledInputRoot} from "./CustomNumberInputStyle";
+import {Box, Typography} from "@mui/material";
 
 const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
     return (
-        <BaseNumberInput
-            slots={{
-                root: StyledInputRoot,
-                input: StyledInput,
-                incrementButton: StyledButton,
-                decrementButton: StyledButton,
-            }}
-            slotProps={{
-                incrementButton: {
-                    children: <AddIcon fontSize="small" />,
-                    className: 'increment',
-                },
-                decrementButton: {
-                    children: <RemoveIcon fontSize="small" />,
-                },
-            }}
-            {...props}
-            ref={ref}
-        />
+   
+            <BaseNumberInput
+                slots={{
+                    root: StyledInputRoot,
+                    input: StyledInput,
+                    incrementButton: StyledButton,
+                    decrementButton: StyledButton,
+                }}
+                slotProps={{
+                    incrementButton: {
+                        children: <AddIcon fontSize="small" />,
+                        className: 'increment',
+                    },
+                    decrementButton: {
+                        children: <RemoveIcon fontSize="small" />,
+                    },
+                }}
+                {...props}
+                ref={ref}
+            />
+   
     );
 });
 
-export default function CustomNumberInput() {
-    return <NumberInput aria-label="Quantity Input" min={1} max={99} />;
+export default function CustomNumberInput( { label}) {
+    const inputId = "custom-number-input";
+    return(
+        <>
+            <Typography variant="caption" component="label" htmlFor={inputId}>
+                {label}
+            </Typography>
+            <NumberInput  id={inputId} aria-label="Number input field" min={1} max={99} />
+        </>
+        
+    )
 }
