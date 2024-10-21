2aa2f46386706f9ac929c9075410c9deb3d45a37
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 13:46:11 +0300

    feat(components): implement CustomNumberInput for creating field of vacation days

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -1,14 +1,15 @@
 ﻿
-import {FormControl, TextField} from "@mui/material";
+import {FormControl, InputLabel, TextField} from "@mui/material";
 import {DatePicker} from "@mui/x-date-pickers";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import CustomNumberInput from "../CustomNumberInput/CustomNumberInput";
 export default function RequestForm() {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <FormControl>
                 <DatePicker label="Start date"/>
-                <TextField type="text" label="Vacation days"></TextField>
+                <CustomNumberInput label="Vacation days" />
                 <DatePicker label="End date" ></DatePicker>
                 <TextField type="text"></TextField>
             </FormControl>
