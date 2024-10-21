49a851ca6995f28e9de1725065ebf26bd14e31da
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 10:58:02 +0300

    fix(components): fix localization context issue
    This error appeared due to not using LocalizationProvider and AdapterDayJS

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -1,13 +1,18 @@
 ﻿
 import {FormControl, TextField} from "@mui/material";
 import {DatePicker} from "@mui/x-date-pickers";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 export default function RequestForm() {
     return (
-        <FormControl>
-            <DatePicker label="Start date"/>
-            <TextField type="text" label="Vacation days"></TextField>
-            <DatePicker label="End date" ></DatePicker>
-            <TextField type="text"></TextField>
-        </FormControl>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <FormControl>
+                <DatePicker label="Start date"/>
+                <TextField type="text" label="Vacation days"></TextField>
+                <DatePicker label="End date" ></DatePicker>
+                <TextField type="text"></TextField>
+            </FormControl>
+            
+        </LocalizationProvider>
     )
 }
