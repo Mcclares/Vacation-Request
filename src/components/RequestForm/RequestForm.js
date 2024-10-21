b9db1e3f4472a36d4ed5ab801721f1a80acec6c3
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 10:28:18 +0300

    feat(components): create RequestForm for submitting vacation request

---
+++ src/components/RequestForm/RequestForm.js
@@ -0,0 +1,13 @@
+﻿
+import {FormControl, TextField,} from "@mui/material";
+
+export default function RequestForm() {
+    return (
+        <FormControl>
+            <TextField type="date" label="Start date"/>
+            <TextField type="text" label="Vacation days"></TextField>
+            <TextField type="date" label="End Date"></TextField>
+            <TextField type="text"></TextField>
+        </FormControl>
+    )
+}
