ac6611a34f47f1a3427831c33d5a73e1248f29b5
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 16:42:39 +0300

    feat(hooks): add logic to calculate vacation days using useVacationDays hook

--- src/hooks/useVacationDays.js
+++ src/hooks/useVacationDays.js
@@ -2,5 +2,14 @@
 
 const useVacationDays = (startDate, endDate) => {
     const [vacationDays, setVacationDays] = useState(0)
-    
+
+    useEffect(() => {
+        if(startDate && endDate) {
+            const daysDiff = endDate.diff(startDate, 'day');
+            setVacationDays(daysDiff)
+        }
+    }, [startDate,endDate]);
+    return vacationDays;
 }
+
+export default useVacationDays;
