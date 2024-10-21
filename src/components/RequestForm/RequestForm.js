8fa62c3d02e9165f603619ce39c2bfbef2d5474e
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 14:50:59 +0300

    feat(components): add attributes in TextField for creating comment field

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -4,14 +4,21 @@ import {DatePicker} from "@mui/x-date-pickers";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import CustomNumberInput from "../CustomNumberInput/CustomNumberInput";
+
 export default function RequestForm() {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <FormControl>
-                <DatePicker label="Start date"/>
-                <CustomNumberInput label="Vacation days" />
-                <DatePicker label="End date" ></DatePicker>
-                <TextField type="text"></TextField>
+            <FormControl >
+                <DatePicker  label="Start date"/>
+                <CustomNumberInput  label="Vacation days" />
+                <DatePicker  label="End date" ></DatePicker>
+                <TextField 
+                    multiline
+                    rows={5} 
+                    variant="outlined"
+                    label="Comment"
+                    helperText="Please leave your comments or suggestions."
+                   />
             </FormControl>
             
         </LocalizationProvider>
