ae7052ad810d9787eeaf9b747251e22e8c7ac882
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-21 14:57:27 +0300

    style(components): implement styles from RequestFormStyle to RequestForm

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -4,20 +4,21 @@ import {DatePicker} from "@mui/x-date-pickers";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import CustomNumberInput from "../CustomNumberInput/CustomNumberInput";
-
+import {FieldStyle} from "./RequestFormStyle";
 export default function RequestForm() {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <FormControl >
-                <DatePicker  label="Start date"/>
-                <CustomNumberInput  label="Vacation days" />
-                <DatePicker  label="End date" ></DatePicker>
+                <DatePicker sx={FieldStyle} label="Start date"/>
+                <CustomNumberInput sx={FieldStyle}  label="Vacation days" />
+                <DatePicker sx={FieldStyle}  label="End date" ></DatePicker>
                 <TextField 
                     multiline
                     rows={5} 
                     variant="outlined"
                     label="Comment"
                     helperText="Please leave your comments or suggestions."
+                    sx={FieldStyle}
                    />
             </FormControl>
             
