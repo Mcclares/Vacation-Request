e163b5b50d61478f2cfb15ec4aa8934c448f9898
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 14:29:53 +0300

    feat(App.js): wrap main application for properly alert working

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import IndexPage from "./pages/IndexPage/IndexPage";
 import FormPage from "./pages/FormPage/FormPage";
-
+import {AlertProvider} from "./hooks/useAlert";
 
 function App() {
   return (
-       <BrowserRouter>
-         <Routes>
-           <Route path="/" element={<IndexPage/>} />
-           <Route path="/requestForm" element={<FormPage/>}/>
-         </Routes>
-       </BrowserRouter>
+      <AlertProvider>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<IndexPage/>} />
+                  <Route path="/requestForm" element={<FormPage/>}/>
+              </Routes>
+          </BrowserRouter>
+      </AlertProvider>
+    
   );
 }
 
