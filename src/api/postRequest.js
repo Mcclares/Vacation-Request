59bb3b7bcdcec73262f876399bdcd08b71b4057f
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 22:41:18 +0300

    fix(api): add comment issue with saving to localStorage

--- src/api/postRequest.js
+++ src/api/postRequest.js
@@ -1,12 +1,13 @@
 ﻿
-export default function postRequest(startDate, vacationDays, endDate) {
+export default function postRequest(startDate, vacationDays, endDate, comment) {
     const requestId = Date.now();
     
     const formData = {
         id: requestId,
         startDate: startDate.format("DD/MM/YYYY"),
         vacationDays: vacationDays,
-        endDate: endDate.format("DD/MM/YYYY")
+        endDate: endDate.format("DD/MM/YYYY"),
+        comment: comment
     }
     
     const existingRequest = JSON.parse(localStorage.getItem('vacationRequests')) || [];
