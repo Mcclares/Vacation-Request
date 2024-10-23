2b70fc3a86eaf6fa490b01806dd6518d1c84acfb
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 14:52:07 +0300

    feat(hooks): add useTimeoutClearEffect hook to handle timer cleanup

---
+++ src/hooks/useTimeOutClearEffect.js
@@ -0,0 +1,11 @@
+﻿import {useEffect} from "react";
+
+export const UseTimeOutClearEffect = (timeOutRef) => {
+    useEffect(() => {
+        return () => {
+            if(timeOutRef.current) {
+                clearTimeout(timeOutRef.current);
+            }
+        }
+    }, [timeOutRef]);
+}
