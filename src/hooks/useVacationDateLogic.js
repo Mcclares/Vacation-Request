4b85fe83570f3be622b2b7e61e82c142ea5749e8
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 15:06:08 +0300

    feat(hooks): add useVacationDateLogic hook to handle dates

---
+++ src/hooks/useVacationDateLogic.js
@@ -0,0 +1,21 @@
+﻿import {useEffect} from "react";
+
+
+export const useVacationDateLogic = (startDate, endDate, vacationDays, setVacationDays, setEndDate, setIsErrorInDates) => {
+    useEffect(() => {
+        if(endDate && endDate.isAfter(startDate,'day')) {
+            const daysDiff = endDate.diff(startDate, 'day');
+            setVacationDays(daysDiff);
+            setIsErrorInDates(false);
+        }
+
+    }, [endDate, startDate, setVacationDays, setIsErrorInDates])
+
+    useEffect(() => {
+        if(startDate && vacationDays) {
+            const calculatedEndDate = startDate.add(vacationDays, 'day');
+            setEndDate(calculatedEndDate)
+            setIsErrorInDates(false);
+        }
+    },[startDate,vacationDays,setEndDate,setIsErrorInDates])
+}
