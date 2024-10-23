5ba8d693c29461f48f2ca967b329e2f5486d84d3
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 10:59:14 +0300

    style(components): change color and add startIcon, EndIcon parameters for styling

--- src/components/Button/СustomButton.js
+++ src/components/Button/СustomButton.js
@@ -1,12 +1,15 @@
 ﻿import {Button as MuiButton} from "@mui/material";
 import {customButtonCommonStyle} from "./CustomButtonStyles";
-export default function CustomButton({name,clickEvent,variant,customStyle, isError}) {
+export default function CustomButton({name,clickEvent,variant,customStyle, isError, startIcon, endIcon}) {
     return(
         <MuiButton 
             style={customStyle?.props || customButtonCommonStyle}  
             variant={variant} 
             onClick={clickEvent}
-            disabled={isError}>
+            disabled={isError}
+            color="#8E8D8A"
+            startIcon={startIcon}
+            endIcon={endIcon}>
             {name}
         </MuiButton>
     )
