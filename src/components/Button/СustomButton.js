0e78b0d181e3b121d399c028d40b1649a6109b7a
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 11:07:48 +0300

    feat(components): add type parameter for correct submitting

--- src/components/Button/СustomButton.js
+++ src/components/Button/СustomButton.js
@@ -1,6 +1,6 @@
 ﻿import {Button as MuiButton} from "@mui/material";
 import {customButtonCommonStyle} from "./CustomButtonStyles";
-export default function CustomButton({name,clickEvent,variant,customStyle, isError, startIcon, endIcon}) {
+export default function CustomButton({name,clickEvent,variant,customStyle, isError, startIcon, endIcon,type}) {
     return(
         <MuiButton 
             style={customStyle?.props || customButtonCommonStyle}  
@@ -9,7 +9,8 @@ export default function CustomButton({name,clickEvent,variant,customStyle, isErr
             disabled={isError}
             color="#8E8D8A"
             startIcon={startIcon}
-            endIcon={endIcon}>
+            endIcon={endIcon}
+            type={type}>
             {name}
         </MuiButton>
     )
