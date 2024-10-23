38acd85dbc6ef9dfda7f256143936f85a8505612
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 14:28:54 +0300

    feat(components): create AlertProvider for managing state of the alert

---
+++ src/components/AlertProvider/AlertProvider.js
@@ -0,0 +1,32 @@
+﻿import {useState} from "react";
+import {Snackbar} from "@mui/material";
+
+const AlertProvider = ({ children }) => {
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [severity, setSeverity] = useState('info');
+
+    const showAlert = (newMessage, newSeverity = 'info') => {
+        setMessage(newMessage);
+        setSeverity(newSeverity);
+        setOpen(true);
+    };
+
+    const hideAlert = () => {
+        setOpen(false);
+    };
+
+    return (
+        <AlertContext.Provider value={showAlert}>
+            {children}
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={hideAlert}
+                message={message}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                severity={severity}
+            />
+        </AlertContext.Provider>
+    );
+};
