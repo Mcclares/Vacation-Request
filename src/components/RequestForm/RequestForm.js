68b4620e40af0a019fcf6960f77c0d1ae120abd5
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 18:04:33 +0300

    fix(components): fix issue with the end of year

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -28,10 +28,6 @@ import postRequest from "../../api/postRequest";
 const MAX_VACATION_DAYS = 28;
 
 export default function RequestForm() {
-
-    const showAlert = useAlert();
-    
-    const goToPage = handleNavigation();
     
     const today = dayjs();
     const tomorrow = today.add(1,'day');
@@ -39,31 +35,41 @@ export default function RequestForm() {
     const endOfYearEndDate = today.endOf('year');
     const endOfYearStartDate = today.endOf('year').subtract(1,'day');
     
-    const [isErrorInDates, setIsErrorInDates] = useState(false);
-    
     const [startDate, setStartDate] = useState(today);
     const [vacationDays, setVacationDays] = useState(1);
     const [endDate,setEndDate] = useState(tomorrow);
+    const [maxEndDay, setMaxEndDay] = useState(startDate.add(MAX_VACATION_DAYS, 'day'));
     
     const nextDayAfterStartDay = startDate.add(1,'day');
     
     const [comment, setComment] = useState('');
     
-    const [maxEndDay, setMaxEndDay] = useState(startDate.add(MAX_VACATION_DAYS, 'day'));
-    
+
+    const [isErrorInDates, setIsErrorInDates] = useState(false);
     const timeoutRef = useRef(null);
+
+    const showAlert = useAlert();
+    const goToPage = handleNavigation();
     
     useTimeOutClearEffect(timeoutRef);
     useVacationDateLogic(startDate, endDate, vacationDays, setVacationDays, setEndDate, setIsErrorInDates);
+
+    useEffect(() => {
+        const intervalYear = startDate.add(1,'year');
+        const potentialMaxEndDate = startDate.add(MAX_VACATION_DAYS, 'day');
+        const calculatedMaxEndDay = potentialMaxEndDate.isAfter(intervalYear)
+            ? intervalYear : potentialMaxEndDate;
+        setMaxEndDay(calculatedMaxEndDay);
+    }, [startDate]);
+    
+    
     
     const handleSubmit = (event) => {
         event.preventDefault()
         if (!isErrorInDates) {
             
             postRequest(startDate,vacationDays,endDate);
-            
             showAlert("Form submitted successfully", "success");
-            
             goToPage("/");
 
         }else {
@@ -80,13 +86,14 @@ export default function RequestForm() {
                         sx={FieldStyle}
                         label="Start date"
                         minDate={today}
+                        defaultValue={today}
                         value={startDate}
                         onChange={(newDate) => {
                             handleStartDateChange(
                                 newDate,
                                 today,
-                                endOfYearStartDate,
-                                endOfYearEndDate,
+                                startDate.add(1,'year'),
+                                startDate.add(1,'year').subtract(1,'day'),
                                 MAX_VACATION_DAYS,
                                 vacationDays,
                                 setVacationDays,
@@ -98,7 +105,7 @@ export default function RequestForm() {
                                 tomorrow
                             )
                         }}
-                        maxDate={endOfYearStartDate}
+                        maxDate={startDate.add(1,'year')}
                         format={"DD/MM/YY"}
                         slotProps={{
                             textField: {
@@ -125,11 +132,11 @@ export default function RequestForm() {
                         required/>
 
                     <DatePicker
-
                         sx={FieldStyle}
                         label="End date"
-                        minDate={nextDayAfterStartDay}
+                        minDate={startDate.add(1,'day')}
                         value={endDate}
+                        defaultValue={tomorrow}
                         maxDate={maxEndDay}
                         onChange={(newDate)=> {
                             handleEndDateChange(
