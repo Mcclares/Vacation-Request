f6649b252c485fc46fd1652d022fcedc5ea0e5d4
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 10:03:27 +0300

    feat(components):  add attribute to CustomButton for validation date.
    If one of dates is incorrect, then disable button

--- src/components/Button/СustomButton.js
+++ src/components/Button/СustomButton.js
@@ -1,11 +1,12 @@
 ﻿import {Button as MuiButton} from "@mui/material";
 import {customButtonCommonStyle} from "./CustomButtonStyles";
-export default function CustomButton({name,clickEvent,variant,customStyle}) {
+export default function CustomButton({name,clickEvent,variant,customStyle, isError}) {
     return(
         <MuiButton 
             style={customStyle?.props || customButtonCommonStyle}  
             variant={variant} 
-            onClick={clickEvent}>
+            onClick={clickEvent}
+            disabled={isError}>
             {name}
         </MuiButton>
     )
