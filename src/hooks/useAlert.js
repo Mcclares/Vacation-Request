8d77f79e6fffa77915162afecbff939d457c2390
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 14:27:31 +0300

    feat(hooks): create hook for creating custom alert

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,44 @@
+﻿import React, {createContext, useContext, useState} from "react";
+import Snackbar from "@mui/material/Snackbar";
+
+
+const AlertContext = createContext();
+
+export const useAlert = () => {
+    const context = useContext(AlertContext);
+    if(!context) {
+        throw new Error("useAlert must be used withing an AlertProvider");
+    }
+    return context;
+};
+
+export const AlertProvider = ({ children }) => {
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [severity, setSeverity] = useState('success');
+    
+    const showAlert = (newMessage, newSeverity = "success") => {
+        setMessage(newMessage);
+        setSeverity(newSeverity);
+        setOpen(true);
+    };
+    
+    const hideAlert = () => {
+        setOpen(false);
+    }
+    
+    return (
+        <AlertContext.Provider value={showAlert}>
+            {children}
+            <Snackbar
+            open = {open}
+            autoHideDuration={6000}
+            onClose={hideAlert}
+            message={message}
+            anchorOrigin={{vertical: 'top', horizontal:'center'}}
+            severity={severity}/>
+        </AlertContext.Provider>
+    )
+    
+    
+}
