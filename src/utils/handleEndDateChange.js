a7ee9744445594bd8b9ca9cb2d2a6d47a0918d57
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-23 18:07:15 +0300

    fix(utils): fix issue with checking MAX_VACATION_DAYS before
    This issue was, when we choose end date last day of 28 days from start date

--- src/utils/handleEndDateChange.js
+++ src/utils/handleEndDateChange.js
@@ -8,7 +8,7 @@ export const handleEndDateChange = (
     setIsErrorInDates,
 ) => {
     const newEndDate = newDate ? dayjs(newDate) : null;
-    if (newEndDate && newEndDate.isAfter(startDate, 'day') && newEndDate.isBefore(maxEndDay, 'day')) {
+    if (newEndDate && newEndDate.isAfter(startDate, 'day') && newEndDate.isBefore(maxEndDay.add(1,'day'), 'day')) {
         setEndDate(newEndDate);
         setIsErrorInDates(false);
     }else {
