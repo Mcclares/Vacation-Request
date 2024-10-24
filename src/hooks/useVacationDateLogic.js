97fa1db9260ce3887b9d8a2f154e2cb3287e2b6a
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-24 15:34:32 +0300

    refactor(hook): simplify vacation date logic

--- src/hooks/useVacationDateLogic.js
+++ src/hooks/useVacationDateLogic.js
@@ -1,8 +1,21 @@
 ﻿import {useEffect, useState} from "react";
 
 
-export const useVacationDateLogic = (startDate, endDate, vacationDays, setVacationDays, setEndDate, endOfYear, setMaxDayCustomInput, MAX_VACATION_DAYS) => {
+export const useVacationDateLogic = (
+    startDate, 
+    endDate, 
+    vacationDays, 
+    setVacationDays, 
+    setEndDate, 
+    endOfYear, 
+    setMaxDayCustomInput, 
+    MAX_VACATION_DAYS
+) => {
     const [isManualEndDate, setIsManualEndDate] = useState(false);
+    
+    const calculatedEndDate = (start, days) => {
+        return start.add(days, 'day').isAfter(endOfYear, 'day') ? endOfYear : start.add(days, 'day');
+    }
     
     useEffect(() => {
         if (endDate && endDate.isAfter(startDate, 'day') && endDate.isBefore(endOfYear)) {
@@ -15,27 +28,15 @@ export const useVacationDateLogic = (startDate, endDate, vacationDays, setVacati
     useEffect(() => {
         if(!isManualEndDate && startDate && vacationDays) {
             const daysUntilEndOfYear = endOfYear.subtract(1,'day').diff(startDate, 'day');
-            if(daysUntilEndOfYear < MAX_VACATION_DAYS) {
-                if(vacationDays > daysUntilEndOfYear) {
-                    setVacationDays(daysUntilEndOfYear);
-                }
-                setMaxDayCustomInput(daysUntilEndOfYear + 1);
-               
-            } 
-            if(vacationDays > MAX_VACATION_DAYS) {
-                setVacationDays(MAX_VACATION_DAYS);
-                setMaxDayCustomInput(MAX_VACATION_DAYS);
-            }
             
-            const calculatedEndDate = startDate.add(vacationDays , 'day');
+            const adjustedVacationDays = Math.min(vacationDays,MAX_VACATION_DAYS, daysUntilEndOfYear);
+            setVacationDays(adjustedVacationDays)
+            setMaxDayCustomInput(adjustedVacationDays);
             
-            if (calculatedEndDate.isAfter(endOfYear, 'day')) {
-                setEndDate(endOfYear);
-            } else {
-                setEndDate(calculatedEndDate);
-            }
+            const newEndDate = calculatedEndDate(startDate, )
+            setEndDate(newEndDate);
         }
-    }, [startDate, vacationDays, isManualEndDate, endOfYear, setEndDate, setVacationDays]);
+    }, [startDate, vacationDays, isManualEndDate, endOfYear, setEndDate, setVacationDays,setMaxDayCustomInput]);
     
     useEffect(() => {
         setIsManualEndDate(false);
