d2f4f7d19509a28c3c93cb3052e6025aa4a9443d
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-24 14:59:12 +0300

    feat(IndexPage): add header for the table

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -2,12 +2,14 @@
 import "./IndexPage.css";
 import CustomButton from "../../components/Button/СustomButton";
 import {handleNavigation} from "../../utils/handleNavigation";
+import {Typography} from "@mui/material";
 
 
 export default function IndexPage() {
     const goToPage = handleNavigation();
     return (
         <div className="index-page">
+            <Typography variant="h4" gutterBottom sx={{ margin: '20px 0'}}> Vacation requests</Typography>
             <VacationList/>
             <CustomButton name="Submit request" variant="contained" clickEvent={()=> goToPage("requestForm") } />
         </div>
