406f65862465485f6a6af98ff78ba8d0b7897ad7
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-24 22:18:12 +0300

    fix(utils): redo the logic for the end date
    If user input invalid date, then block submit button

--- src/utils/handleEndDateChange.js
+++ src/utils/handleEndDateChange.js
@@ -1,39 +1,27 @@
 ﻿import dayjs from 'dayjs';
-
 export const handleEndDateChange = (
-    today,
     newDate,
     startDate,
-    setStartDate,
     setEndDate,
-    setIsErrorInDates,
-    setVacationDays,
-    timeoutRef,
+    setIsInvalidDate,
+    showAlert,
     MAX_VACATION_DAYS,
 ) => {
     const newEndDate = newDate ? dayjs(newDate) : null;
     const newMaxEndDay = startDate.add(MAX_VACATION_DAYS, 'day');
-    const tomorrow = today.add(1, 'day');
-
-    if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
+    
+    
+    if(newEndDate && newEndDate.isAfter(newMaxEndDay, 'day')) {
+        setIsInvalidDate(true);
+        showAlert("Error: Invalid date selection", "error")
+        
+    }else if( newEndDate && newEndDate.isBefore(startDate.add(1,'day'),'day')) {
+        setIsInvalidDate(true);
+        showAlert("Error: Invalid date selection", "error")
+     
+    } else {
+        setEndDate(newEndDate);
+        setIsInvalidDate(false);
     }
-    timeoutRef.current = setTimeout( () => {
-        if(newEndDate && newEndDate.isAfter(newMaxEndDay, 'day')) {
-            setStartDate(today);
-            setVacationDays(1);
-            setEndDate(tomorrow);
-            setIsErrorInDates(true);
-        }else if( newEndDate && newEndDate.isBefore(startDate.add(1,'day'),'day')) {
-            setStartDate(today);
-            setVacationDays(1);
-            setEndDate(tomorrow);
-            setIsErrorInDates(true);
-            setEndDate(newEndDate)
-            setIsErrorInDates(false);
-        } else {
-            setEndDate(newEndDate);
-        }
-    }, 500)
     
 };
