448a3a5fc0a8632a525a87d16b78269451fd63f3
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-25 15:35:08 +0300

    feat: add check for undefined value in localStorage is empty

--- src/components/RowContent/RowContent.js
+++ src/components/RowContent/RowContent.js
@@ -5,23 +5,32 @@ import {columns} from "../columnsConfig";
 import React from "react";
 
 export function rowContent(handleCommentClick, row) {
+    if(!row) {
+        return null;
+    }
+    
+    
     return (
         <React.Fragment>
-            {columns.map((column) => (
+            {columns.map((column) => {
+                const cellValue = row[column.dataKey];
+                
+                return (
                 <TableCell
                     key={column.dataKey}
                     align={column.numeric ? 'right' : 'left'}
                     onClick={
-                        column.dataKey === 'comment' && row[column.dataKey]?.trim() !== ''
-                            ? () => handleCommentClick(row[column.dataKey])
+                        column.dataKey === 'comment' && cellValue?.trim() !== ''
+                            ? () => handleCommentClick(cellValue)
                             : null
                     }
-                    style={{ cursor: getCursorStyle(column, row) }}
+                    style={{cursor: getCursorStyle(column, row)}}
                     sx={tableCellStyle}
                 >
                     {row[column.dataKey]}
-                </TableCell>
-            ))}
+                </TableCell>);
+                    
+            })}
         </React.Fragment>
     );
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,7 @@
 ﻿export function getCursorStyle(column, row) {
+    if(!row || !column.dataKey || !(column.dataKey in row)) {
+        return "default";
+    }
     const cellValue = row[column.dataKey];
     if (column.dataKey === 'comment' && cellValue && cellValue.trim() !== '') {
         return 'pointer';
