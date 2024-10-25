8bf174c554bea36c043c4dde2b6bfecd3f6ae507
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-10-25 14:51:04 +0300

    refactor(components): change name isError to isInvalidDate for better understanding

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,12 +1,12 @@
 ﻿import {Button as MuiButton} from "@mui/material";
 import {customButtonCommonStyle} from "./CustomButtonStyles";
-export default function CustomButton({name,clickEvent,variant,customStyle, isError, startIcon, endIcon,type}) {
+export default function CustomButton({name,clickEvent,variant,customStyle, isInvalidDate, startIcon, endIcon,type}) {
     return(
         <MuiButton
             style={customStyle?.props || customButtonCommonStyle}
             variant={variant}
             onClick={clickEvent}
-            disabled={isError}
+            disabled={isInvalidDate}
             color="#8E8D8A"
             startIcon={startIcon}
             endIcon={endIcon}
